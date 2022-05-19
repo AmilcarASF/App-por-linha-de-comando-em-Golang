ad1b7f2c71d342f35de11b0b1bbc62cad3aabf55
AmilcarFerreira <amilcar.ferreira.easysoft.com.br>
2022-05-19 10:41:14 -0300

    app linha de comando Golang

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # Exemplo de App por linha de comando em Golang
 
-- Para listar IPs (deve passar "ip" e a URL por parametro, para listar os IPs da URL), exemplo:
-go run main.go ip google.com
+-> Para listar IPs (deve passar "ip" e a URL por parametro, para listar os IPs da URL), exemplo:
+ - go run main.go ip google.com
 
 
-- Para listar Servidores (deve passar servidores" e a URL por parametro, para listar os Servidores da URL), exemplo:
-go run main.go servidores google.com
+-> Para listar Servidores (deve passar servidores" e a URL por parametro, para listar os Servidores da URL), exemplo:
+
+ - go run main.go servidores google.com
---
+++ app/app.go
@@ -0,0 +1,67 @@
+package app
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/urfave/cli"
+)
+
+func Gerar() *cli.App {
+	app := cli.NewApp()
+	app.Name = "Aplicação de Linha de Comando"
+	app.Usage = "Busca IPs e Nomes de Servidor na internet"
+
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "google.com",
+		},
+	}
+
+	app.Commands = []cli.Command{
+		{
+			Name:   "ip",
+			Usage:  "Busca IPs de endereços na internet",
+			Flags:  flags,
+			Action: buscarIps,
+		},
+		{
+			Name:   "servidores",
+			Usage:  "Busca o nome do servidor na internet",
+			Flags:  flags,
+			Action: buscarServidores,
+		},
+	}
+	return app
+}
+
+// a funv vai pegar o host que está vindo por parametro e buscar os Ips
+func buscarIps(c *cli.Context) {
+	host := c.String("host") // pega o valor da flag host
+
+	// busca por ips
+	ips, erro := net.LookupIP(host) // retorna um slice de ips
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
+
+}
+
+func buscarServidores(c *cli.Context) {
+	host := c.String("host")
+
+	servidores, erro := net.LookupNS(host)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, servidor := range servidores {
+		fmt.Println(servidor.Host)
+	}
+}
